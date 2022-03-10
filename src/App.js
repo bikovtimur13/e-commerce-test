1f59df16e37e7c77e5a2cea1028712f6fa22943f
Timur Bykov <bikov.timur.013@gmail.com>
2022-03-10 14:20:20 +0300

    ADD:simple-data

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import data from './data';
 
 function App() {
   return (
@@ -7,10 +8,30 @@ function App() {
         <a href="/">amazona</a>
       </header>
       <main>
-        list products
+        <h1>Featured Products</h1>
+        <div className="products">
+          {
+            data.products.map(product => (
+              <div className="product" key={product.slug}>
+                <a href={`/product/${product.slug}`}>
+                  <img src={product.image} alt={product.name} />
+                </a>
+                <div className="product-info">
+                  <a href={`/product/${product.name}`}>
+                    <p>{product.name}</p>
+                  </a>
+                  <p>
+                    <strong>${product.price}</strong>
+                  </p>
+                  <button>Add to cart</button>
+                </div>
+              </div>
+            ))
+          }
+        </div>
       </main>
     </div>
   );
-}
+};
 
 export default App;
