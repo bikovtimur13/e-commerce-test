97077b085161df57a751f8064dfc9273a73e228f
Timur Bykov <bikov.timur.013@gmail.com>
2022-03-28 10:22:09 +0300

    ADD:CartScreen

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { useContext } from 'react';
 import './App.css';
 import HomeScreen from './components/HomeScreen';
 import ProductScreen from './components/ProductScreen';
+import CartScreen from './components/CartScreen';
 import { Navbar, Container, Nav, Badge } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import { Store } from './Store';
@@ -37,6 +38,7 @@ function App() {
           <Container className="mt-3">
             <Routes>
               <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="cart" element={<CartScreen />} />
               <Route path="/" element={<HomeScreen />} />
             </Routes>
           </Container>
