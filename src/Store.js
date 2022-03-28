ae5b56c6b8d9a0179414dbfdacda50649671c623
Timur Bykov <bikov.timur.013@gmail.com>
2022-03-28 11:34:00 +0300

    ADD: add to cart functionality

--- src/Store.js
+++ src/Store.js
@@ -4,12 +4,14 @@ export const Store = createContext();
 
 const initialState = {
     cart: {
-        cartItems: [],
-    }
+        cartItems: localStorage.getItem('cartItems')
+            ? JSON.parse(localStorage.getItem('cartItems'))
+            : [],
+    },
 };
 function reducer(state, action) {
     switch (action.type) {
-        case 'CART_ADD_ITEM':
+        case 'CART_ADD_ITEM': {
             //add to cart
             const newItem = action.payload;
             const existItem = state.cart.cartItems.find(
@@ -20,7 +22,16 @@ function reducer(state, action) {
                     item._id === existItem._id ? newItem : item
                 )
                 : [...state.cart.cartItems, newItem];
-            return { ...state, cart: { ...state.cart, cartItems } };
+            localStorage.setItem('cartItems', JSON.stringify(cartItems));
+            return { ...state, cart: { ...state.cart, cartItems } }
+        };
+        case 'CART_REMOVE_ITEM': {
+            const cartItems = state.cart.cartItems.filter(
+                (item) => item._id !== action.payload._id
+            );
+            localStorage.setItem('cartItems', JSON.stringify(cartItems));
+            return { ...state, cart: { ...state.cart, cartItems } }
+        };
         default:
             return state;
     };
